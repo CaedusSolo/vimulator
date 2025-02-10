8e5cee49070885474e33bb55c8442136edfd2939
Ying Tong <yingtong0106@gmail.com>
2025-02-10 16:14:23 +0800

    Created cursor motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [cursorVisible, setCursorVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCursorVisible(prevState => !prevState)
+    },650)
+    return () => clearInterval(interval);
+  }, [])
+
+
   return (
     <>
       <h1 className="text-center">Vimulator</h1>
       <div className="container">
         <div className="game-board">
-          This is the game board
+          <div className="cursor" style={{display: cursorVisible ? "block" : "none"}}>
+            &nbsp;
+          </div>
         </div>
       </div>
     </>
