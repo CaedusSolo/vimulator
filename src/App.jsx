f2ae6dc3a5a7ab43ddd91f857e67a84bdb496d86
Ying Tong <yingtong0106@gmail.com>
2025-03-03 23:43:11 +0800

    Chore: Refactor project to include React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,141 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import GameBoard from './pages/GameBoard';
 
 function App() {
-  const [cursorVisible, setCursorVisible] = useState(true);
-  const [cursorRow, setCursorRow] = useState(0);
-  const [cursorColumn, setCursorColumn] = useState(0);
-  const [targetRow, setTargetRow] = useState(Math.floor(Math.random() * 12));
-  const [targetColumn, setTargetColumn] = useState(Math.floor(Math.random() * 37));
-
-  useEffect(() => {
-    resetChallenge();
-    const interval = setInterval(() => {
-      setCursorVisible((prevState) => !prevState);
-    }, 550);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  });
-
-  useEffect(() => {
-    const cursor = document.querySelector('.cursor');
-    if (cursor) {
-      cursor.style.gridRowStart = cursorRow + 1; 
-      cursor.style.gridColumnStart = cursorColumn + 1; 
-    }
-    console.log(`Cursor Row: ${cursorRow}`)
-    console.log(`Cursor Column: ${cursorColumn}`)
-  }, [cursorRow, cursorColumn]);
-
-  useEffect(() => {
-    const target = document.querySelector('.target');
-    if (target) {
-      target.style.gridRowStart = targetRow + 1; 
-      target.style.gridColumnStart = targetColumn + 1; 
-    }
-    console.log(`Target Row: ${targetRow}`)
-    console.log(`Target Column: ${targetColumn}`)
-  }, [targetRow, targetColumn]);
-
-  useEffect(() => {
-    checkCollision();
-  }, [cursorRow, cursorColumn]); 
-
-  function handleKeyDown(e) {
-    const key = e.key;
-
-    if (key === 'h') {
-      moveCursorLeft();
-    } else if (key === 'j') {
-      moveCursorDown();
-    } else if (key === 'k') {
-      moveCursorUp();
-    } else if (key === 'l') {
-      moveCursorRight();
-    }
-  }
-
-  function moveCursorUp() {
-    setCursorRow((prevRow) => Math.max(0, prevRow - 1));
-    const cursor = document.querySelector('.cursor');
-    cursor.style.backgroundColor = 'lightcoral';
-    setTimeout(() => {
-      cursor.style.backgroundColor = 'red';
-    }, 300);
-  }
-
-  function moveCursorDown() {
-    setCursorRow((prevRow) => Math.min(12, prevRow + 1));
-    const cursor = document.querySelector('.cursor');
-    cursor.style.backgroundColor = 'lightcoral';
-    setTimeout(() => {
-      cursor.style.backgroundColor = 'red';
-    }, 300);
-  }
-
-  function moveCursorLeft() {
-    setCursorColumn((prevColumn) => Math.max(0, prevColumn - 1));
-    const cursor = document.querySelector('.cursor');
-    cursor.style.backgroundColor = 'lightcoral';
-    setTimeout(() => {
-      cursor.style.backgroundColor = 'red';
-    }, 300);
-  }
-
-  function moveCursorRight() {
-    setCursorColumn((prevColumn) => Math.min(37, prevColumn + 1));
-    const cursor = document.querySelector('.cursor');
-    cursor.style.backgroundColor = 'lightcoral';
-    setTimeout(() => {
-      cursor.style.backgroundColor = 'red';
-    }, 300);
-  }
-
-  function checkCollision() {
-    if (cursorRow === targetRow && cursorColumn === targetColumn) {
-      console.log("Collided! Resetting challenge")
-      resetChallenge();
-    }
-  }
-
-  function resetChallenge() {
-    setTargetRow(Math.floor(Math.random() * 12));
-    setTargetColumn(Math.floor(Math.random() * 37));
-  }
-
   return (
-    <>
-      <h1 className="text-center">Vimulator</h1>
-      <h1 className="text-center">Get to the X !</h1>
-
-      <div className="container">
-        <div className="game-board">
-          <div
-            className="cursor"
-            style={{
-              display: cursorVisible ? 'block' : 'none',
-              gridRowStart: cursorRow + 1,
-              gridColumnStart: cursorColumn + 1,
-            }}
-          >
-            &nbsp;
-          </div>
-            <i className="fa-solid fa-x target" 
-            style={{
-              gridRowStart: targetRow + 1,
-              gridColumnStart: targetColumn + 1
-            }}
-            ></i>
-        </div>
-      </div>
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="game" element={<GameBoard />} />
+          <Route path="auth">
+            <Route path="login" />
+            <Route path="sign-up" />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
