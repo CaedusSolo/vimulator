b93f8d6e81a07fd973514d2219917f8fb4257b34
Ying Tong <yingtong0106@gmail.com>
2025-03-04 23:42:21 +0800

    chore: Redid styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,28 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      Nav
+    <nav className="navbar sticky-top">
+      <div className="links-container">
+
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `nav-link active` : "nav-link"
+          }
+          to="/"
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `nav-link active` : "nav-link"
+          }
+          to="/"
+          end
+        >
+          Home
+        </NavLink>
+      </div>
     </nav>
   )
 }
