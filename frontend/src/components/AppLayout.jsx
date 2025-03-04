25a4d8a71b0f220f2012edbe9881dc83fbd3474a
Ying Tong <yingtong0106@gmail.com>
2025-03-04 23:01:38 +0800

    chore: Refactor directory structure

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
 function AppLayout() {
   return (
     <div className="container">
-      
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
