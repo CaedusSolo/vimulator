dd4f19fbf2518e0efb0bb246d894c15e5729c439
Ying Tong <yingtong0106@gmail.com>
2025-03-05 16:35:51 +0800

    feature: Setup Supabase connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-//  backend entry point
+//  imports
+import express from "express"
+import cors from "cors"
+import supabase from "./config/supabase.js"
+
+//  setup
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+app.use(cors())
+
+console.log("SUPABASE DETAILS HERE:")
+console.log(supabase)
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
