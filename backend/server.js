cbcd4a105f572c3263d3a584949b8c9f4b668adc
Ying Tong <yingtong0106@gmail.com>
2025-03-06 13:48:48 +0800

    feature: Added backend boilerplate for auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //  imports
 import express from "express"
 import cors from "cors"
+import authRouter from "./routes/authRoutes.js"
 import supabase from "./config/supabase.js"
 
 //  setup
@@ -9,6 +10,7 @@ const PORT = 5000
 
 app.use(express.json())
 app.use(cors())
+app.use("/auth", authRouter)
 
 console.log("SUPABASE DETAILS HERE:")
 console.log(supabase)
