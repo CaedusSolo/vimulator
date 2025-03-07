17b56a7d87bae7fa723a224e0a5429d8b7d9678a
Ying Tong <yingtong0106@gmail.com>
2025-03-07 16:42:05 +0800

    feature: Added login and sign up pages

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <h2 className="text-center">Welcome to Vimulator</h2>   
+      
+    </>
+  )
+}
+
+export default LandingPage
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
 function SignUpPage() {
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault() 
+    console.log("Form submitted!")
+  }
+
   return (
-    <>
-      <h2 className="text-center">Sign Up Page</h2>
-    </>
+    <div className="content-container mx-auto w-75">
+      <h2 className="text-center mb-4">Sign Up</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="form-group mb-2">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input className="form-control"  type="password" name="password" id="password" minLength={8} />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="confirm-password" className="form-label">Confirm Password</label>
+          <input className="form-control" type="password" name="confirm-password" id="confirm-password" minLength={8} />
+        </div>
+      </form>
+    </div>
   )
 }
 
