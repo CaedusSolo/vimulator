7c3fd5deb60faafb4bc170861b782b99b97c896f
Ying Tong <yingtong0106@gmail.com>
2025-03-13 23:38:50 +0800

    feat: Added content to landing page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p className="text-center mt-2">Copyright 2025 Chow Ying Tong</p>
     </footer>
   )
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <>
+    <div className="content-container landing-page">
       <h2 className="text-center">Welcome to Vimulator</h2>   
-      
-    </>
+      <h3 className="text-center">Practice Vim commands here</h3>
+      <div className="btn-container">
+        <button className="btn">Game</button>
+        <button className="btn">Leaderboard</button>
+      </div>
+    </div>
   )
 }
 
