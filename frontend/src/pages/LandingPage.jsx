8f6a43f5aee03ce15abe5b5161396a0c6c7d35da
Ying Tong <yingtong0106@gmail.com>
2025-03-14 15:35:16 +0800

    fix: Fix broken nav links

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function LandingPage() {
   return (
     <div className="content-container landing-page">
-      <h2 className="text-center">Welcome to Vimulator</h2>   
+      <h2 className="text-center">Welcome to Vimulator</h2>
       <h3 className="text-center">Practice Vim commands here</h3>
       <div className="btn-container">
-        <button className="btn">Game</button>
-        <button className="btn">Leaderboard</button>
+        <button className="btn">
+          <Link to="/game" className="link">Game</Link>
+        </button>
+        <button className="btn">
+          <Link to="/leaderboard" className='link'>Leaderboard</Link>
+        </button>
       </div>
     </div>
   )
